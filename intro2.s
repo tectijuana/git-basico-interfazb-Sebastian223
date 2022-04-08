6685556673cbab9eca99d08551162eee8b93d450
Sebastian Gonzalez Elogio <77412917+Sebastian223@users.noreply.github.com>
2022-04-08 14:28:10 -0700

    Add files via upload

---
+++ intro2.s
@@ -0,0 +1,15 @@
+# Title: intro2					Filename: holamundo.s
+# Author:Sebastian Gonzalez Elogio				Date: 08/04/2022
+.data
+var1 : .byte 0b00110010
+.align
+var2 : .byte 0b11000000
+.align
+.text
+.global main
+main : ldr r1, = var1 /* r1 <- & var1 */
+ldrsb r1, [ r1 ] /* r1 <- *r1 */
+ldr r2, = var2 /* r2 <- & var2 */
+ldrsb r2, [ r2 ] /* r2 <- *r2 */
+add r0, r1, r2 /* r0 <- r1 + r2 */
+bx lr
