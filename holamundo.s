e9a8ed9c08facc3cfc3246023a7bbfdcb6222528
Sebastian Gonzalez Elogio <77412917+Sebastian223@users.noreply.github.com>
2022-04-08 14:18:13 -0700

    Add files via upload

---
+++ holamundo.s
@@ -0,0 +1,20 @@
+# Title: Hola mundo en ARM					Filename: holamundo.s
+# Author:Sebastian Gonzalez Elogio				Date: 08/04/2022
+# Description: Mostrar HOLA MUNDO
+# Input: -
+# Output: HOLA MUNDO
+
+.global _start
+############################# Code segment ####################################
+_start:
+  MOV R7, #4 ###;- cargar el valor de datos que esta en esta posicion en la secuencia de instrucciones
+  MOV R0, #1 ###;- cargar el valor de datos que esta en esta posicion en la secuencia de instrucciones
+  MOV R2, #12 ###;- parametro
+  LDR R1, =string ###parametro y direccion de la cadena
+  SWI 0 ### señal de interrupcion al O.S.
+  MOV R7, #1 ### salir del programa y matar el proceso
+  SWI 0 ### señal de interrupcion al O.S. para la terminacion
+############################# Data segment ###################################
+  .data
+string:
+  .ascii "HOLA MUNDO"
